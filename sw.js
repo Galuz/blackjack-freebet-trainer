5aa974acb8f9d8afdb16da888e8d975da87216df
German Gonzalez <german.glz01@gmail.com>
2026-09-15 18:58:18 -0600

    tune: slow down deal animation cadence
    * tune: slow down casino deal cadence
    
    * chore: bump PWA cache for slower deal timing

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='freebet21-v14';
-const ANIMATION_SRC='./deal-animations.js?v=14';
+const CACHE='freebet21-v15';
+const ANIMATION_SRC='./deal-animations.js?v=15';
 const ASSETS=['./manifest.webmanifest','./icon.svg',ANIMATION_SRC];
 
 function withAnimationLoader(html){
